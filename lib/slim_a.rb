4005b927c57e67f1be521950dcb7649c6c0ff9a2
tungnt1203 <tungnguyen120301@gmail.com>
2025-01-22 10:31:00 +0700

    init commit

---
+++ lib/slim_a.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "slim_a/version"
+
+module SlimA
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/slim_a_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SlimA do
+  it "has a version number" do
+    expect(SlimA::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
